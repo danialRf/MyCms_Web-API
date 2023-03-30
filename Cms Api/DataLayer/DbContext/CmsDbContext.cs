c3d94f67bf0cfa11f986474ab40af7c5ffe5dcd3
MohammadReza <m.pourghorban2004@gmail.com>
2023-03-30 21:27:22 +0430

    Add Package

---
+++ Cms Api/DataLayer/DbContext/CmsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyCmsWebApi.DataLayer.DbContext;
+
+public class CmsDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public CmsDbContext()
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CmsDbContext).Assembly);
+    }
+}
