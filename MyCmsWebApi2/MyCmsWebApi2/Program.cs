1955648f325ae06f3a1a8028ddfc7e806d54283a
Danial <D.roshanfekr@gmial.com>
2023-04-05 14:44:55 +0430

    Serilog

--- MyCmsWebApi2/MyCmsWebApi2/Program.cs
+++ MyCmsWebApi2/MyCmsWebApi2/Program.cs
@@ -3,12 +3,17 @@ using Microsoft.Extensions.Configuration;
 using MyCmsWebApi2.DataLayer.Context;
 using MyCmsWebApi2.DataLayer.Repository;
 using MyCmsWebApi2.DataLayer.Services;
+using Serilog;
 using System.Configuration;
 
 
 
 
-
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -34,6 +39,13 @@ var connectionString = configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<CmsDbContext>(options => options.UseSqlServer(connectionString));
 
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddSerilog(dispose: true);
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -42,6 +54,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
