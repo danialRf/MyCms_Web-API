1e3127ff74b9e873c0f26e83345ae40c15900d85
Sadra Seifi <sadrayzseifi@gmail.com>
2023-04-08 18:25:41 +0430

    Added CommentController Added Comment Controller admin Added DateTimeNow to COmment mode Added commentExist to ICommentRepository and it's service Added commentDto and show commentdto

--- MyCmsWebApi2/MyCmsWebApi2/DataLayer/Repository/ICommentsRepository.cs
+++ MyCmsWebApi2/MyCmsWebApi2/DataLayer/Repository/ICommentsRepository.cs
@@ -6,10 +6,9 @@ namespace MyCmsWebApi2.DataLayer.Repository;
     public interface ICommentRepository
     {
         Task<List<Comments>> GetAllAsync();  
-
         Task<Comments> GetCommentByIdAsync(int id);
         Task<Comments> InsertCommentAsync(Comments comment);
-        Task UpdateCommentAsync(Comments comment);
         Task DeleteCommentByIdAsync(int id);
-    }
+        Task<bool> CommentExist(int id);
+}
 
--- MyCmsWebApi2/MyCmsWebApi2/Profiles/NewsGroupProfile.cs
+++ MyCmsWebApi2/MyCmsWebApi2/Profiles/NewsGroupProfile.cs
@@ -12,8 +12,6 @@ namespace MyCmsWebApi2.Profiles
             CreateMap<NewsGroup, ShowNewsGroupDto>();
             CreateMap<AddNewsGroupDto,NewsGroup>();
             CreateMap<EditNewsGroupDto, NewsGroup>();
-            
-
         }
     }
 }
