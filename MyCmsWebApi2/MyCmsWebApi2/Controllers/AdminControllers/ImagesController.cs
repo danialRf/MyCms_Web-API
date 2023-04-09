2b6f454a1726a6eb6218f4831fb88353fa36d22c
Danial <D.roshanfekr@gmial.com>
2023-04-09 11:48:23 +0430

    Images

--- MyCmsWebApi2/MyCmsWebApi2/Controllers/AdminControllers/ImagesController.cs
+++ MyCmsWebApi2/MyCmsWebApi2/Controllers/AdminControllers/ImagesController.cs
@@ -1,58 +1,54 @@
-﻿using AutoMapper;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MyCmsWebApi2.DataLayer.Repository;
-using MyCmsWebApi2.Dtos.ImagesDto;
-using MyCmsWebApi2.Dtos.NewsDto;
-using MyCmsWebApi2.Dtos.NewsGroupDto;
+﻿//using AutoMapper;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using MyCmsWebApi2.DataLayer.Repository;
+//using MyCmsWebApi2.Dtos.ImagesDto;
+//using MyCmsWebApi2.Dtos.NewsDto;
+//using MyCmsWebApi2.Dtos.NewsGroupDto;
 
-namespace MyCmsWebApi2.Controllers.AdminControllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ImagesController : ControllerBase
-    {
-        private readonly IImageRepository _imageRepository;
-        private readonly IMapper _mapper;
+//namespace MyCmsWebApi2.Controllers.AdminControllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class ImagesController : ControllerBase
+//    {
+//        private readonly IImageRepository _imageRepository;
+//        private readonly IMapper _mapper;
+//        private readonly string _imagePath = Path.Combine(Directory.GetCurrentDirectory(), "Images");
+//        public class ImageService : IImageService
+//        {
+//            private readonly string _imagePath;
 
-        public ImagesController(IImageRepository imageRepository, IMapper mapper)
-        {
-            this._imageRepository = imageRepository;
-            _mapper = mapper;
-        }
+//            public ImageService(IWebHostEnvironment env, IConfiguration config)
+//            {
+//                _imagePath = Path.Combine(env.ContentRootPath, config["ImageSettings:ImagePath"]);
+//            }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ShowImagesDto>>> GetAllImagesAsync()
-        {
-            try
-            {
-                var news = await _imageRepository.GetAllAsync();
-                if (news == null)
-                {
-                    return NotFound();
-                }
-                var imagesDtos = _mapper.Map<List<ShowImagesDto>>(news);
-
-                return Ok(imagesDtos);
-            }
-            catch (Exception)
-            {
-
-                return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred during getting all pagesasync");
-            }
+//            public ImagesController(IImageRepository imageRepository, IMapper mapper)
+//        {
+//            this._imageRepository = imageRepository;
+//            _mapper = mapper;
+//        }
 
 
 
-            [HttpGet("{id}")]
-            public async Task<ActionResult<ShowImagesDto>> GetImagesByIdAsync(int id)
-            {
-                if (await _imageRepository.ImageExist(id) == false)
-                    return NotFound();
 
-                var result = await _imageRepository.GetImageByIdAsync(id);
+//        [HttpGet("images/{imageName}")]
+//        public IActionResult GetImage(string imageName)
+//        {
 
-                return Ok(_mapper.Map<ShowImagesDto>(result));
-            }
-        }
-    }
-}
+//            var imagePath = Path.Combine(_imagePath, imageName);
+
+//            if (!System.IO.File.Exists(imagePath))
+//            {
+//                return NotFound();
+//            }
+
+//            var imageFile = System.IO.File.OpenRead(imagePath);
+//            return File(imageFile, "image/jpeg");
+//        }
+
+
+//    }
+
+//}
